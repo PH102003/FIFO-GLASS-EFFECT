14e2efc2f2ede93734387d2ba9d01f0f6e5d2264
PH102003 <pedroh.rj2003@hotmail.com>
2024-04-06 19:44:44 -0300

    add

---
+++ script.js
@@ -0,0 +1,6 @@
+let fila = [];
+
+const inputNumero = document.getElementById("inputNumero");
+const btAdd = document.getElementById("btAdd");
+const btRemover = document.getElementById("btRemover");
+const btLimpar = document.getElementById("btLimpar");
