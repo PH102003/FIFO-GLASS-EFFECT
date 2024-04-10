9d44185e6871e911e0344ed69ce35c3c8292f68d
PH102003 <pedroh.rj2003@hotmail.com>
2024-04-10 16:57:51 -0300

    add

--- script.js
+++ script.js
@@ -1,51 +1,52 @@
 let fila = [];
 
-let objetoExibicao = {};
 const inputNome = document.getElementById("inputNome");
 const btAdd = document.getElementById("btAdd");
 const btRemover = document.getElementById("btRemover");
-const btLimpar = document.getElementById("btLimpar");
+const listaDaFila = document.getElementById("listaDaFila");
 
-//no evento de "click" (mouse) do botão add, botao remover, ou botao limpar, chama as funções respectivas
-btAdd.addEventListener("click", adicionarNome);
-btRemover.addEventListener("click", removerNome);
-btLimpar.addEventListener("click", limparFila);
+//no evento de "click" (mouse) do botão add ou botao remover, chama as funções respectivas
+btAdd.addEventListener("click", handleAdicionarNome);
+btRemover.addEventListener("click", handleRemoverNome);
 
 
-const adicionarNome = () => {
+const handleAdicionarNome = () => {
     //verifica se a entrada não foi vazia  
-    if(inputNome !== ''){
-        fila.push(inputNome);
+    if (inputNome.value !== '') {
+        fila.push(inputNome.value);
+        //limpa o campo de entrada
+        refreshObjeto();
+        document.getElementById('inputNome').value = '';
+        inputNome.value = "";
+        inputNome.focus();
     }
-    //limpa o campo de entrada
-    refreshObjeto();
-    document.getElementById('inputNome').value = '';
+
 }
 /*quando ele clicar no botao de remover, ele deve automaticamente
 remover o primeiro elemento*/
-const removerNome = () => {
-    //verifica se tem elementos na fila
-    if(fila.length > 0){
-        fila.shift(inputNome);
+const handleRemoverNome = () => {
+    //verifica se tem elementos na fila pra remover
+    if (fila.length > 0) {
+        fila.shift();
         refreshObjeto();
+    }
 
-    }
-    
-    
+
 }
-function limparFila() {
-    // Limpa toda a fila
-    fila.length = 0;
-    // Atualiza o objeto de exibição
-    atualizarObjetoExibicao();
-}
-/*limpa o conteúdo anterior e, em seguida, 
-itera sobre todas as propriedades do objeto objetoExibicao, 
-exibindo elas no elemento html
-*/
+
+
 const refreshObjeto = () => {
-    preObjeto.innerHTML = "";
-    for (const campo in objetoExibicao) {
-      preObjeto.innerHTML += `${campo} = ${objetoExibicao[campo]}\n`;
-    }
-  };
+    listaDaFila.innerHTML = ""; // limpa a listaDaFila
+    fila.forEach((nome, index) => { // itera a fila
+            /*
+          na criação da constante "textNode", 
+          ele se utiliza da transformação em strings de acordo com os dados de nome,
+           onde o nome deverá ser exibido na instância do "objetoExibicao" para mostrar o nome da pessoa na fila, 
+           após  isso o document.createElement("li"), faz literalmente a criação da tag <li>, por fim, 
+          o appendChild, faz com que os elementos de textNode sejam exibidos na tag <li>
+        */
+        const li = document.createElement("li"); //cria a tag <li> 
+        li.textContent = nome; // Define o texto do elemento li
+        listaDaFila.appendChild(li); // Adiciona o li à lista
+    });
+}
